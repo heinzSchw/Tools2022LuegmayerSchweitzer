4a72254404936ca41223f563734bc8f7c023a5ab
Heinz Schweitzer <heinz.schweitzer1@gmx.net>
2022-05-17 10:25:09 +0200

    add some code

--- LuegM_heinzSchw_android/test.java
+++ LuegM_heinzSchw_android/test.java
@@ -2,7 +2,7 @@ public void thisIsANewMethod(){
 
     System.out.println("tic tac toe")
     System.out.println("Hey whats up?");
-
+    Int var = System.In.readInt();
 }
 
 public static void main(String[]args){
