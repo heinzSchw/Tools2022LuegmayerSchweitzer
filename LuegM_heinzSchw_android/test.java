9adce585901a9371eaf3c13a6e37770d6b8114f7
Heinz Schweitzer <heinz.schweitzer1@gmx.net>
2022-05-17 09:42:59 +0200

    conflicts solved

--- LuegM_heinzSchw_android/test.java
+++ LuegM_heinzSchw_android/test.java
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
 public void thisIsANewMethod(){
 
         }
-=======
 public static void main(String[]args){
     System.out.println("test");
 }
->>>>>>> b7152a46de41a52a4d9be9faca647d7cdaa17122
